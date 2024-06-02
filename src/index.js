27d73dc02cb0c652f77b5dbaed4c7743f2908ed1
IvanVasylenko <ivasylenko@ukr.net>
2024-06-03 00:59:10 +0300

    Change router provider

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import * as ReactDOMClient from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { PersistGate } from 'redux-persist/integration/react'
 import { appStore, persistor } from './store';
 
@@ -13,11 +13,11 @@ const rootElement = document.getElementById('root')
 const root = ReactDOMClient.createRoot(rootElement)
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={appStore}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
       </PersistGate>
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 )
